c125cf802de82395f2b2e6571271d3bf917ba595
maxharchik\DESKTOP-56V3UQQ <maxharchik@gmail.com>
2023-11-18 21:40:25 -0800

    Fixed file path error

--- Tracks/MH - Track Visuals Manager Settings.lua
+++ Tracks/MH - Track Visuals Manager Settings.lua
@@ -5,7 +5,7 @@
 ----------------------------------------
 r = reaper
 local scriptName = ({ r.get_action_context() })[2]:match("([^/\\_]+)%.[Ll]ua$")
-tvm = r.GetResourcePath() .. '/Scripts/MH Scripts/Track/MH - Track Style Manager Globals.lua'; if r.file_exists(tvm) then dofile(tvm); if not tvm then r.ShowMessageBox("This script requires a newer version of the MH Scripts repositiory!\n\n\nPlease resync from the above menu:\n\nExtensions > ReaPack > Synchronize Packages", "Error", 0); return end else r.ShowMessageBox("This script requires the full MH Scripts repository!\n\nPlease visit github.com/mharchik/ReaperScripts for more information", "Error", 0); return end
+tvm = r.GetResourcePath() .. '/Scripts/MH Scripts/Track/MH - Track Visuals Manager Globals.lua'; if r.file_exists(tvm) then dofile(tvm); if not tvm then r.ShowMessageBox("This script requires a newer version of the MH Scripts repositiory!\n\n\nPlease resync from the above menu:\n\nExtensions > ReaPack > Synchronize Packages", "Error", 0); return end else r.ShowMessageBox("This script requires the full MH Scripts repository!\n\nPlease visit github.com/mharchik/ReaperScripts for more information", "Error", 0); return end
 if not mh.SWS() or not mh.JS() then mh.noundo() return end
 ----------------------------------------
 --Script Variables
