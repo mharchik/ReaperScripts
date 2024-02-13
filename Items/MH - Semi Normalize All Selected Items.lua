f715fa7e50d07ca15e0f908dcc44a832e37e59fd
maxharchik <maxharchik@gmail.com>
2024-02-12 16:48:55 -0800

    Adding normalization script

--- Items/MH - Semi Normalize All Selected Items.lua
+++ Items/MH - Semi Normalize All Selected Items.lua
@@ -1,5 +1,4 @@
 ----------------------------------------
--- @noindex
 -- @description Semi Normalize All Selected Items
 -- @author Max Harchik
 -- @version 1.0
@@ -13,7 +12,7 @@ r = reaper
 local scriptName = ({ r.get_action_context() })[2]:match('([^/\\_]+)%.[Ll]ua$')
 mh = r.GetResourcePath() .. '/Scripts/MH Scripts/Functions/MH - Functions.lua'; 
 if r.file_exists(mh) then dofile(mh); if not mh or mh.version() < 1.0 then r.ShowMessageBox('This script requires a newer version of the MH Scripts repositiory!\n\n\nPlease resync from the above menu:\n\nExtensions > ReaPack > Synchronize Packages', 'Error', 0); return end else r.ShowMessageBox('This script requires the full MH Scripts repository!\n\nPlease visit github.com/mharchik/ReaperScripts for more information', 'Error', 0); return end
-if not mh.SWS() or not mh.JS() then mh.noundo() return end
+if not mh.SWS() then mh.noundo() return end
 ----------------------------------------
 --User Settings
 ----------------------------------------
